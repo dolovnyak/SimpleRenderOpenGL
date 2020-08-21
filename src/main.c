72d30a40f4f5e103a3c174bb4ad8e80573badb15
dolovnyak <dolovnyak@yandex.ru>
2020-08-21 06:32:42 +0300

    add libraries glew and glfw, and now window is creating

--- src/main.c
+++ src/main.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true,
+	// и приложение после этого закроется
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main()
 {
-	glfwDefaultWindowHints();
+	glfwInit();
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		printf("Failed to create GLFW window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Failed to initialize GLEW");
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	glClearColor(0.8f, 0.3f, 0.3f, 1.0f);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+		glfwSetKeyCallback(window, key_callback);
+	}
+
 	printf("Hello, World!\n");
+	glfwTerminate();
 	return 0;
 }
