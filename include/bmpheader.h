abc94bf8a2d07cb7d205ce0d2e7a38aa9d3895e9
dolovnyak <dolovnyak@yandex.ru>
2020-09-18 00:20:35 +0300

    added bmp loader

--- include/bmpheader.h
+++ include/bmpheader.h
@@ -2,26 +2,25 @@
 #define BPM_HEADER_H
 
 #include <stdint.h>
-#include <stdio.h>
 #include "libft.h"
 
-typedef struct	s_bmpheader
+typedef struct		s_bmpheader
 {
-	uint16_t	bfType;				// 0x4d42 | 0x4349 | 0x5450
-	int			bfSize;				// размер файла
-	int			bfReserved;			// 0
-	int			bfOffBits;			// смещение до поля данных, обычно 54 = 16 + biSize
-	int			biSize;				// размер струкуры в байтах: 40(BITMAPINFOHEADER) или 108(BITMAPV4HEADER) или 124(BITMAPV5HEADER)
-	int			biWidth;			// ширина в точках
-	int			biHeight;			// высота в точках
-	u_int16_t	biPlanes;			// всегда должно быть 1
-	u_int16_t	biBitCount;			// 0 | 1 | 4 | 8 | 16 | 24 | 32
-	int			biCompression;		// BI_RGB | BI_RLE8 | BI_RLE4 | BI_BITFIELDS | BI_JPEG | BI_PNG (реально используется лишь BI_RGB)
-	int			biSizeImage;		// Количество байт в поле данных (обычно устанавливается в 0)
-	int			biXPelsPerMeter;	// горизонтальное разрешение, точек на дюйм
-	int			biYPelsPerMeter;	// вертикальное разрешение, точек на дюйм
-	int			biClrUsed;			// Количество используемых цветов (если есть таблица цветов)
-	int			biClrImportant;		// Количество существенных цветов (можно считать, просто 0)
-}				t_bmpheader;
+	uint16_t		file_type;				// 0x4d42 | 0x4349 | 0x5450
+	unsigned int	file_size;				// размер файла
+	unsigned int	reserved;			// 0
+	unsigned int	offset_bits;			// смещение до поля данных, обычно 54 = 16 + biSize
+	int				info_header_size;				// размер струкуры в байтах: 40(BITMAPINFOHEADER) или 108(BITMAPV4HEADER) или 124(BITMAPV5HEADER)
+	int				width;			// ширина в точках
+	int				height;			// высота в точках
+	u_int16_t		planes;			// всегда должно быть 1
+	u_int16_t		bit_per_pixel;			// 0 | 1 | 4 | 8 | 16 | 24 | 32
+	unsigned int	compression;		// BI_RGB | BI_RLE8 | BI_RLE4 | BI_BITFIELDS | BI_JPEG | BI_PNG (реально используется лишь BI_RGB)
+	unsigned int	image_size;		// Количество байт в поле данных (обычно устанавливается в 0)
+	unsigned int	x_pixels_per_meter;	// горизонтальное разрешение, точек на дюйм
+	unsigned int	y_pixels_per_meter;	// вертикальное разрешение, точек на дюйм
+	unsigned int	used_color;			// Количество используемых цветов (если есть таблица цветов)
+	unsigned int	color_important;		// Количество существенных цветов (можно считать, просто 0)
+}					t_bmpheader;
 
 #endif
--- src/load_bmp.c
+++ src/load_bmp.c
@@ -1,45 +1,51 @@
+#include <fcntl.h>
+#include <stdio.h>
 #include "bmpheader.h"
 #include "scop.h"
 
 t_bmpheader	read_and_check_bmpheader(FILE *file, const char *filename)
 {
-	t_bmpheader	bmpheader;    // File header sizeof(BMPheader) = 56
+	t_bmpheader	bmpheader;
 	size_t		res;
 
-	res = fread(&bmpheader, 1, sizeof(t_bmpheader), file);
-	if (res != sizeof(t_bmpheader))
-	{
-		fclose(file);
-		ft_exit_with_error((const char *[]){"BMP FILE HEADER: ", filename, " DOESN'T CORRECT"}, -1);
-	}
-	if (bmpheader.bfType != 0x4d42 && bmpheader.bfType != 0x4349 && bmpheader.bfType != 0x5450)
-	{
-		fclose(file);
-		ft_exit_with_error((const char *[]){"BMP FILE HEADER: ", filename, " DOESN'T CORRECT"}, -1);
-	}
+	fread(&bmpheader.file_type, 2, 1, file);
+	fread(&bmpheader.file_size, 4, 1, file);
+	fread(&bmpheader.reserved, 4, 1, file);
+	fread(&bmpheader.offset_bits, 4, 1, file);
+	fread(&bmpheader.info_header_size, 4, 1, file);
+	fread(&bmpheader.width, 4, 1, file);
+	fread(&bmpheader.height, 4, 1, file);
+	fread(&bmpheader.planes, 2, 1, file);
+	fread(&bmpheader.bit_per_pixel, 2, 1, file);
+	fread(&bmpheader.compression, 4, 1, file);
+	fread(&bmpheader.image_size, 4, 1, file);
+	fread(&bmpheader.x_pixels_per_meter, 4, 1, file);
+	fread(&bmpheader.y_pixels_per_meter, 4, 1, file);
+	fread(&bmpheader.used_color, 4, 1, file);
+	res = fread(&bmpheader.color_important, 4, 1, file);
+
+	bmpheader.width = bmpheader.width < 0 ? -bmpheader.width : bmpheader.width;
+	bmpheader.height = bmpheader.height < 0 ? -bmpheader.height : bmpheader.height;
+
+//	fseek(file, 0, SEEK_END);
+//	int file_size = ftell(file);
+//
+//	if (res == 0 ||
+//	(bmpheader.file_type != 0x4d42 && bmpheader.file_type != 0x4349 && bmpheader.file_type != 0x5450) ||
+//	bmpheader.file_size != file_size ||
+//	bmpheader.reserved != 0 ||
+//	bmpheader.planes != 1 ||
+//	(bmpheader.info_header_size != 40 && bmpheader.info_header_size != 108 && bmpheader.info_header_size != 124) ||
+//	bmpheader.bit_per_pixel != 24 ||    //only full color
+//	bmpheader.compression != 0     //only not compressed
+//		)
+//	{
+//		fclose(file);
+//		ft_exit_with_error((const char *[]){"BMP FILE HEADER: ", filename, " DOESN'T CORRECT", NULL}, -1);
+//	}
+
+//	fclose(file);
 	return bmpheader;
-}
-
-int			is_bmp_file_correct(FILE *file, t_bmpheader bmpheader)
-{
-	int file_size;
-
-	file_size = ftell(file);
-	fseek(file, 0, SEEK_END);
-	fseek(file, sizeof(t_bmpheader), SEEK_SET);
-	if (bmpheader.bfSize != file_size ||
-		bmpheader.bfReserved != 0 ||
-		bmpheader.biPlanes != 1 ||
-		(bmpheader.biSize != 40 && bmpheader.biSize != 108 && bmpheader.biSize != 124) ||
-		bmpheader.bfOffBits != 14 + bmpheader.biSize ||
-
-		bmpheader.biWidth < 1 || bmpheader.biWidth > 10000 ||
-		bmpheader.biHeight < 1 || bmpheader.biHeight > 10000 ||
-		bmpheader.biBitCount != 24 ||    //only full color
-		bmpheader.biCompression != 0     //only not compressed
-		)
-		return (0);
-	return (1);
 }
 
 t_texture load_bmp(const char *filename)
@@ -48,50 +54,32 @@ t_texture load_bmp(const char *filename)
 	FILE			*file;
 	t_bmpheader		bmpheader;
 	size_t			res;
-	int				mx3 = (3 * texture.width + 3) & (-4);    // Compute row width in file, including padding to 4-byte boundary
-	unsigned char	*tmp_buf = ft_memalloc(mx3 * texture.height);
+
 
 	file = fopen(filename, "rb");
 	if (!file)
 		ft_exit_with_error((const char *[])
-		{"FILE PATH: ", filename, " DOESN'T CORRECT", NULL}, -1);
-	bmpheader = read_and_check_bmpheader(file, filename);
-	if (!is_bmp_file_correct(file, bmpheader))
-	{
-		fclose(file);
-		ft_exit_with_error((const char *[])
-		{"BMP FILE: ", filename, " DOESN'T CORRECT FORMAT", NULL}, -1);
-	}
+								   {"FILE PATH: ", filename, " DOESN'T CORRECT", NULL}, -1);
 
-	texture.width = bmpheader.biWidth;
-	texture.height = bmpheader.biHeight;
-	res = fread(tmp_buf, 1, mx3 * texture.height, file);
-	if ((int) res != mx3 * texture.height)
+	bmpheader = read_and_check_bmpheader(file, filename);
+
+
+	bmpheader.image_size = bmpheader.width * bmpheader.height * (bmpheader.bit_per_pixel / 8);
+	unsigned char *image = (unsigned char*)malloc(bmpheader.image_size + 1);
+//	rewind(file);
+	fseek(file, bmpheader.offset_bits, SEEK_SET);
+	fread(image, 1, bmpheader.image_size, file);
+
+	for (int imageidx = 0;imageidx < bmpheader.image_size; imageidx+=3) // fixed semicolon
 	{
-		fclose(file);
-		ft_exit_with_error((const char *[])
-		{"BMP FILE: ", filename, " DOESN'T CORRECT", NULL}, -1);
+		unsigned char temprgb = image[imageidx];
+		image[imageidx] = image[imageidx + 2];
+		image[imageidx + 2] = temprgb;
 	}
+	texture.width = bmpheader.width;
+	texture.height = bmpheader.height;
+	texture.image = image;
+
 	fclose(file);
 	return texture;
-
-// выделим память для результата
-//	v = new int[mx * my];
-
-	// Перенос данных (не забудем про BGR->RGB)
-//	unsigned char *ptr = (unsigned char *) v;
-//	for (int y = my - 1; y >= 0; y--)
-//	{
-//		unsigned char *pRow = tmp_buf + mx3 * y;
-//		for (int x = 0; x < mx; x++)
-//		{
-//			*ptr++ = *(pRow + 2);
-//			*ptr++ = *(pRow + 1);
-//			*ptr++ = *pRow;
-//			pRow += 3;
-//			ptr++;
-//		}
-//	}
-//	delete[] tmp_buf;
-//	return v;    // OK
 }
